01fe6c41f7735c6cccfbf699137b04ce2397b46a
Charles Beall <wacharlb@gmail.com>
2017-08-29 02:40:59 -0700

    Initial Commit

---
+++ Assets/Crossy Road/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveDistance = 1;
+    public float moveTime = 0.4f;
+    public float colliderDistCheck = 1;
+    public bool isIdle = true;
+    public bool isDead = false;
+    public bool isMoving = false;
+    public bool isJumping = false;
+    public bool jumpStart = false;
+    public ParticleSystem particle = null;
+    public GameObject chick = null;
+    private Renderer renderer = null;
+    private bool isVisible = false;
+}
