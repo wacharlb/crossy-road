730ae9b8a479f68d790b1c1e47903c381dcce4ed
Charles Beall <wacharlb@gmail.com>
2017-09-08 03:44:59 -0700

    Added spawner and spawner controller scripts to control spawning objects into the game. Added prefabs for roads, tracks and water.

---
+++ Assets/Crossy Road/Scripts/SpawnController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public bool goLeft = false;
+    public bool goRight = false;
+    public List<GameObject> items = new List<GameObject>();
+    public List<Spawner> spawnersLeft = new List<Spawner>();
+    public List<Spawner> spawnersRight = new List<Spawner>();
+
+    // Use this for initialization
+    void Start ()
+    {
+        int itemId = Random.Range(0, items.Count);
+        GameObject item = items[itemId];
+
+        int direction = Random.Range(0, 2);
+
+        // Set the direction of the item
+        if(direction > 0)
+        {
+            goLeft = false;
+            goRight = true;
+        }
+        else
+        {
+            goLeft = true;
+            goRight = false;
+        }
+
+        for(int i=0; i<spawnersLeft.Count; i++)
+        {
+            spawnersLeft[i].item = item;
+            spawnersLeft[i].goLeft = goLeft;
+            spawnersLeft[i].gameObject.SetActive(goRight);
+            spawnersLeft[i].spawnLeftPos = spawnersLeft[i].transform.position.x;
+        }
+
+        for (int i = 0; i < spawnersRight.Count; i++)
+        {
+            spawnersRight[i].item = item;
+            spawnersRight[i].goLeft = goLeft;
+            spawnersRight[i].gameObject.SetActive(goLeft);
+            spawnersRight[i].spawnRightPos = spawnersRight[i].transform.position.x;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
